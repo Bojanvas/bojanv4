cd436130f21acb32f4600c3ed1929a30a55c124b
Bojanvas <bojan87vasilevski@gmail.com>
2017-02-24 18:41:11 +0100

    Add files via upload

--- public/function.js
+++ public/function.js
@@ -1,6 +1,108 @@
 $(document).ready(function() {
+    var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+    setInterval(rotate, 4000);
+
+    function rotate() {
+        $('.logo i').addClass('rotate').delay(1000).queue(function(next) {
+            $(this).removeClass('rotate');
+            next();
+        })
+    }
+
+    $(window).scroll(function() {
+        var top = $('body').scrollTop();
+        console.log(top);
+        console.log(width);
+        if (width > 1380) {
+            console.log('desktop');
+            if (top > 1320) {
+                $(document).ready(function() {
+                    $('.skillbar').each(function() {
+                        $(this).find('.skillbar-bar').animate({
+                            width: $(this).attr('data-percent')
+                        }, 6000);
+                    });
+                });
+            }
+            if (top > 1900) {
+                $('#first').fadeIn();
+                $('#first').addClass('slideInLeft');
+            }
+            if (top > 2350) {
+                $('#second').fadeIn();
+                $('#second').addClass('slideInRight');
+            }
+            if (top > 2700) {
+                $('#three').fadeIn();
+                $('#three').addClass('slideInLeft');
+            }
+        }
+        // laptop
+        if (width > 1000) {
+            console.log('laptop');
+            if (top > 1320) {
+                $(document).ready(function() {
+                    $('.skillbar').each(function() {
+                        $(this).find('.skillbar-bar').animate({
+                            width: $(this).attr('data-percent')
+                        }, 6000);
+                    });
+                });
+            }
+            if (top > 2000) {
+                $('#first').fadeIn();
+                $('#first').addClass('slideInLeft');
+            }
+            if (top > 2350) {
+                $('#second').fadeIn();
+                $('#second').addClass('slideInRight');
+            }
+            if (top > 2700) {
+                $('#three').fadeIn();
+                $('#three').addClass('slideInLeft');
+            }
+        }
+        // mobile
+        if (width < 600) {
+            console.log('Mobile');
+            if (top > 2700) {
+                $(document).ready(function() {
+                    $('.skillbar').each(function() {
+                        $(this).find('.skillbar-bar').animate({
+                            width: $(this).attr('data-percent')
+                        }, 6000);
+                    });
+                });
+            }
+            if (top > 3150) {
+                $('#first').fadeIn();
+                $('#first').addClass('slideInLeft');
+            }
+            if (top > 3350) {
+                $('#second').fadeIn();
+                $('#second').addClass('slideInRight');
+            }
+            if (top > 3500) {
+                $('#three').fadeIn();
+                $('#three').addClass('slideInLeft');
+            }
+        }
+
+    })
 
 
-
-
+    $(function() {
+        $('a[href*="#"]:not([href="#"])').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
 })
--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,39 @@
 var express = require('express');
-var bodyparser = require('body-parser');
+var body = require('body-parser');
 var path = require('path');
-
+var nodemailer = require('nodemailer');
 var router = express.Router();
 
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    secure: false,
+    requireTLS: true,
+    auth: {
+        user: process.env.USER,
+        pass: process.env.PASS
+    },
+    tls: {
+        rejectUnauthorized: false
+    }
+})
 router.get("/", function(req, res) {
     res.sendFile(path.resolve(__dirname + '/../views/index.html'));
 })
-
+router.get('/mail', function(req, res) {
+    var person = req.body;
+    var mailOptions = {
+        to: 'bojan87vasilevski@gmail.com',
+        from: person.email,
+        subject: "From" + person.name + " " + person.surname,
+        text: person.email + " message is :" + person.message
+    }
+    transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('message send');
+        res.redirect('/');
+    })
+})
 module.exports = router;
