2097b7bc3c7eb9717e5da25b14ffd7f1298e220b
Bojanvas <bojan87vasilevski@gmail.com>
2017-06-13 20:19:16 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var body = require('body-parser');
 var router = require('./routes/routes.js');
+var mongoose = require('mongoose');
 var app = express();
 var port = process.env.PORT || 3000;
 
+
+mongoose.connect("mongodb://" + DB.USER + ":" + DB.PASS + "@ds125262.mlab.com:25262/couterview", function() {
+    console.log('db-conected');
+});
 app.use(express.static(__dirname + '/public'))
 app.use('/', router);
 app.use(body.json());
